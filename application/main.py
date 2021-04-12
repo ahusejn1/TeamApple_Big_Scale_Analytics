ea6aa2cb8451c6c766ff50404db29f361606ba33
ahusejn1 <72014493+ahusejn1@users.noreply.github.com>
2021-04-12 11:57:48 +0200

    Update main.py

--- application/main.py
+++ application/main.py
@@ -1,44 +1,25 @@
 import datetime, sys
 
 from google.cloud import automl_v1
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from google.api_core.client_options import ClientOptions
-
-def inline_text_payload(file_path):
-  with open(file_path, 'rb') as ff:
-    content = ff.read()
-  return {'text_snippet': {'content': content, 'mime_type': 'text/plain'} }
-
-
-def get_prediction(file_path, model_name):
-  options = ClientOptions(api_endpoint='eu-automl.googleapis.com')
-  prediction_client = automl_v1.PredictionServiceClient(client_options=options)
-
-  payload = {'text_snippet': {'content': "Aime les chats", 'mime_type': 'text/plain'} }
-  # Uncomment the following line (and comment the above line) if want to predict on PDFs.
-  # payload = pdf_payload(file_path)
-
-  params = {}
-  request = automl_v1.PredictRequest(name=model_name, payload=payload, params=params)
-  response = prediction_client.predict(request)
-  return response  # waits until request is returned
-
-
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
 
+@app.route('/index/')
+def index():
+	return render_template('index.html')
+
 @app.route('/evaluate', methods = ['GET', 'POST'])
 def evaluate():
-    phrase = ''
+	phrase = ''
     if request.method == "POST":
       phrase = request.form.get('phrase')
-    return redirect ('/index')
-
-@app.route('/')
-def root():
-    return render_template('index.html')
+      niveau = predict.predict(phrase)
+      top_score = niveau.payload[0].display_name
+		return phrase = top_score
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
