72885ea2a2ce73fded48b556b3cec769a366487a
ahusejn1 <72014493+ahusejn1@users.noreply.github.com>
2021-04-12 15:35:03 +0200

    Update main.py

--- application/main.py
+++ application/main.py
@@ -6,6 +6,27 @@ from google.api_core.client_options import ClientOptions
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']='bsakey.json'
+client_options = {'api_endpoint': 'eu-automl.googleapis.com:443'}
+
+project_id = 'unique-grid-305713'
+mpdel_id = 'TCN4988515041545289728'
+prediction_client = automl.PredictionServiceClient(client_options=client_options)
+model_full_id = automl.AutoMlClient.model_path(project_id, "eu", model_id)
+
+def predict(content):
+  text_snippet = automl.TextSnippet(content=content, mime_type= "text/plain")
+  payload = automl.ExamplePayload(text_snippet=text_snippet)
+  response = prediction_client.predict(name=model_full_id, payload=payload)
+  return response
+
+y = predict('Salut')
+
+top_score = y.payload[0].classification.score
+top_cat = y.payload[0].display_name
+
+print(topcat + '& ' + top_score)
+
 app = Flask(__name__)
 
 @app.route('/index/')
@@ -20,8 +41,9 @@ def evaluate():
     niveau = predict(phrase)
     top_score = niveau.payload[0].classification.score
     level = niveau.payload[0].display_name
+    phrase = level
 
-  return render_template('index.html', phrase = level)
+  return render_template('index.html', phrase = phrase)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
